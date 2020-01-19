48d6af4dfb2c35bb6958ce5e64dbc4a47cb171db
Kaitlyn Parkhurst <symkat@symkat.com>
2020-01-19 01:08:41 -0800

    0.1.0 Inital Version
    - Runs shell commands defined in .dex.yaml or dex.yaml.
    - Provides a menu with the description of commands.
    - This repo dog-foods itself.

---
+++ lib/App/dex.pm
@@ -0,0 +1,39 @@
+package App::dex;
+
+our $VERSION = '0.001000'; # 0.1.0
+$VERSION = eval "$VERSION";
+
+=encoding utf8
+
+=head1 NAME
+
+App::dex - Directory Execute
+
+=head1 DESCRIPTION
+
+B<dex> provides a command line utility for managing directory-specific commands.
+
+=head1 USAGE
+
+=head1 DEX FILE SPECIFICATION
+
+=head1 AUTHOR
+
+Kaitlyn Parkhurst (SymKat) I<E<lt>symkat@symkat.comE<gt>> ( Blog: L<http://symkat.com/> )
+
+=head1 CONTRIBUTORS
+
+=head1 SPONSORS
+
+=head1 COPYRIGHT
+
+Copyright (c) 2019 the App::dex L</AUTHOR>, L</CONTRIBUTORS>, and L</SPONSORS> as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms as perl itself.
+
+=head2 AVAILABILITY
+
+The most current version of App::dec can be found at L<https://github.com/symkat/App-dex>
+
