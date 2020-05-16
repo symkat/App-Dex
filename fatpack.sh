6e17dc8c60f706a5872806930b5f549f9fa833c1
Ryan Voots <simcop2387@simcop2387.info>
2020-05-16 10:10:12 -0700

    Use fatpacked script for install by default

--- fatpack.sh
+++ fatpack.sh
@@ -5,15 +5,15 @@ set -u
 set -x
 set -e
 
-mkdir -p packed
+mkdir -p scripts
 export PERL5LIB="$PWD/lib"
-fatpack trace scripts/dex
+fatpack trace scripts.bare/dex
 fatpack packlists-for $(cat fatpacker.trace) > packlists
 fatpack tree $(cat packlists)
-rm packed/dex </dev/null || echo "No packed dex present"
+rm scripts/dex </dev/null || echo "No packed dex present"
 
 # There's warnings here about Class::XSAccessor from Moo, but this is fine.  It'll fall back to pure perl code when that's not installed to the local perl
-fatpack file scripts/dex > packed/dex
-chmod a+rx-w packed/dex
+fatpack file scripts.bare/dex > scripts/dex
+chmod a+rx-w scripts/dex
 
 rm -rf fatpacker.trace packlists fatlib
